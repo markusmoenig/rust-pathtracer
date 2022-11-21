20b387260c2361581272c1497623b66236c8390f
Markus Moenig <markus@moenig.io>
2022-11-21 18:52:22 +0700

    Ported some more disney functions

--- rust-pathtracer/src/tracer.rs
+++ rust-pathtracer/src/tracer.rs
@@ -123,8 +123,18 @@ impl Tracer {
 
                 if in_shawdow == false {
 
-                    ld += PTF3::new(0.5, 0.5, 0.5);
+                    //ld += PTF3::new(0.1, 0.1, 0.1);
 
+                    scatter_sample.f = self.disney_eval(state, -ray[1], &state.ffnormal, &light_sample.direction, &mut scatter_sample.pdf);
+
+                    let mut mis_weight = 1.0;
+                    if analytical.light.area > 0.0 {// No MIS for distant light
+                        mis_weight = self.power_heuristic(&light_sample.pdf, &scatter_sample.pdf);
+                    }
+
+                    if scatter_sample.pdf > 0.0 {
+                        ld += mis_weight * li.component_mul(&scatter_sample.f) / light_sample.pdf;
+                    }
                 }
             }
         }
@@ -181,5 +191,120 @@ impl Tracer {
         }
 
     }
+
+    #[inline(always)]
+    fn power_heuristic(&self, a: &PTF, b: &PTF) -> PTF {
+        let t = a * a;
+        return t / (b * b + t);
+    }
+
+    #[inline(always)]
+    fn mix_ptf(&self, a: &PTF, b: &PTF, v: &PTF) -> PTF {
+        (1.0 - v) * a + b * v
+    }
+
+    // Disney
+
+    fn luminance(&self, c: &PTF3) -> PTF {
+        0.212671 * c.x + 0.715160 * c.y + 0.072169 * c.z
+    }
+
+    fn schlick_fresnel(&self, u: PTF) -> PTF {
+        let m = (1.0 - u).clamp(0.0, 1.0);
+        let m2 = m * m;
+        m2 * m2 * m
+    }
+
+    fn dielectric_fresnel(&self, cos_theta_i: PTF, eta: PTF) -> PTF {
+        let sin_theta_tsq = eta * eta * (1.0 - cos_theta_i * cos_theta_i);
+
+        // Total internal reflection
+        if sin_theta_tsq > 1.0 {
+            return 1.0;
+        }
+
+        let cos_theta_t = (1.0 - sin_theta_tsq).max(0.0).sqrt();
+
+        let rs = (eta * cos_theta_t - cos_theta_i) / (eta * cos_theta_t + cos_theta_i);
+        let rp = (eta * cos_theta_i - cos_theta_t) / (eta * cos_theta_i + cos_theta_t);
+
+        0.5 * (rs * rs + rp * rp)
+    }
+
+    fn get_spec_color(&self, material: &Material, eta: PTF, spec_col: &mut PTF3, sheen_col: &mut PTF3) {
+        let lum = self.luminance(&material.base_color);
+        let ctint = if lum > 0.0 { material.base_color / lum } else { PTF3::new(1.0, 1.0, 1.0) };
+        let f0 = (1.0 - eta) / (1.0 + eta);
+        *spec_col = glm::mix(&(f0 * f0 * glm::mix(&PTF3::new(1.0, 1.0, 1.0), &ctint, material.specular_tint)), &material.base_color, material.metallic);
+        *sheen_col = glm::mix(&PTF3::new(1.0, 1.0, 1.0), &ctint, material.sheen_tint);
+    }
+
+    fn get_lobe_probabilities(&self, material: &Material, _eta: &PTF, spec_col: &PTF3, approx_fresnel: PTF, diffuse_wt: &mut PTF, spec_reflect_wt: &mut PTF,  spec_refract_wt: &mut PTF, clearcoat_wt: &mut PTF)
+    {
+        *diffuse_wt = self.luminance(&material.base_color) * (1.0 - material.metallic) * (1.0 - material.spec_trans);
+        *spec_reflect_wt = self.luminance(&glm::mix(&spec_col, &PTF3::new(1.0, 1.0, 1.0), approx_fresnel));
+        *spec_refract_wt = (1.0 - approx_fresnel) * (1.0 - material.metallic) * material.spec_trans * self.luminance(&material.base_color);
+        *clearcoat_wt = 0.25 * material.clearcoat * (1.0 - material.metallic);
+        let total_wt = *diffuse_wt + *spec_reflect_wt + *spec_refract_wt + *clearcoat_wt;
+
+        *diffuse_wt /= total_wt;
+        *spec_reflect_wt /= total_wt;
+        *spec_refract_wt /= total_wt;
+        *clearcoat_wt /= total_wt;
+    }
+
+    fn disney_fresnel(&self, material: &Material, eta: PTF, ldot_h: PTF, vdot_h: PTF) -> PTF {
+        let metallic_fresnel = self.schlick_fresnel(ldot_h);
+        let  dielectric_fresnel = self.dielectric_fresnel(vdot_h.abs(), eta);
+        self.mix_ptf(&dielectric_fresnel, &metallic_fresnel, &material.metallic)
+    }
+
+    fn disney_eval(&self, state: &State, v: PTF3, n: &PTF3, l: &PTF3, bsdf_pdf: &mut PTF) -> PTF3 {
+        *bsdf_pdf = 0.0;
+        let f = PTF3::zeros();
+
+        fn onb(n: &PTF3, t: &mut PTF3, b: &mut PTF3) {
+            let up = if n.z.abs() < 0.999 { PTF3::new(0.0, 0.0, 1.0) } else { PTF3::new(1.0, 0.0, 0.0) };
+
+            *t = glm::normalize(&glm::cross(&up, &n));
+            *b = glm::cross(&n, &t);
+        }
+
+        fn to_local(x: &PTF3, y: &PTF3, z: &PTF3, v: &PTF3) -> PTF3 {
+                PTF3::new(glm::dot(v, x), glm::dot(v, y), glm::dot(v, z))
+        }
+
+        let mut t = PTF3::zeros();
+        let mut b = PTF3::zeros();
+
+        onb(n, &mut t, &mut b);
+        let v = to_local(&t, &b, n, &v); // NDotL = L.z; NDotV = V.z; NDotH = H.z
+        let l = to_local(&t, &b, n, l);
+
+        let mut h;
+        if l.z > 0.0 {
+            h = glm::normalize(&(l + v));
+        } else {
+            h = glm::normalize(&(l + v * state.eta));
+        }
+
+        if h.z < 0.0 {
+            h = -h;
+        }
+
+        // Specular and sheen color
+        let mut spec_col = PTF3::zeros();
+        let mut sheen_col = PTF3::zeros();
+        self.get_spec_color(&state.material, state.eta, &mut spec_col, &mut sheen_col);
+
+        let mut diffuse_wt = 0.0; let mut spec_reflect_wt = 0.0; let mut spec_refract_wt = 0.0; let mut clearcoat_wt = 0.0;
+
+        let fresnel = self.disney_fresnel(&state.material, state.eta, glm::dot(&l, &h), glm::dot(&v, &h));
+        self.get_lobe_probabilities(&state.material, &state.eta, &spec_col, fresnel, &mut diffuse_wt, &mut spec_reflect_wt, &mut spec_refract_wt, &mut clearcoat_wt);
+
+
+        f
+    }
+
 
 }
